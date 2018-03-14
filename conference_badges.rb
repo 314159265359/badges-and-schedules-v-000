a4cba8abb5587aad5aa5a6a5cd50a4a0eca31663
Karen G <kgraves0701@gmail.com>
2018-03-14 22:59:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,10 +11,13 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(name)
-  counter = 0
+  # counter = 0
   room_array = []
-  while counter < name.count
-    room_array << "Hello, #{name[counter]}! You'll be assigned to room #{counter}!"
-  end
+  # while counter < name.count
+  #   room_array << "Hello, #{name[counter]}! You'll be assigned to room #{counter}!"
+  # end
+  name.each_with_index do |value, index|
+    room_array << "Hello, #{value}! You'll be assigned to room #{index+1}!"
+end
   room_array
 end
